accb92bf40b0114c5baaddba8494df26536cdbc3
Carlos Mejia <cdmejia4@utpl.edu.ec>
2023-04-24 21:53:59 -0500

    subida media

--- Writer01/src/semana3/arrays.java
+++ Writer01/src/semana3/arrays.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package semana3;
-
+import java.util.Arrays;
 /**
  *
  * @author SALA I
@@ -12,8 +12,15 @@ public class arrays {
 
     public static void main(String[] args) {
         
+        //int [] arr = new int [2];
+        //arr [0]= 1;
+        //arr [1]=2;
+       
+        int array [] = {2,4,5,6,1};
         
-        
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array [i]);
+        }
         
         
     }
