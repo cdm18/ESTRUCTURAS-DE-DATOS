d91e617cde040edcc976253971645790f290014a
Luis <ljmora1@utpl.edu.ec>
2023-06-05 18:51:58 -0500

    subida clase

--- Clases/src/semana4/Writer02.java
+++ Clases/src/semana4/Writer02.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package writers;
+package semana4;
 
 import java.io.IOException;
 import java.io.*;
