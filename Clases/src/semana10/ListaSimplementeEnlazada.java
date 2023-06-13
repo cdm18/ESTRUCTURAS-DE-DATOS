df094f033f8c77ef715f9b24dbbb75d186781c55
--replace-all <cdmejia4@utpl.edu.ec>
2023-06-12 21:18:19 -0500

    clase lunes listas simplemente enlazadas

---
+++ Clases/src/semana10/ListaSimplementeEnlazada.java
@@ -0,0 +1,47 @@
+package semana10;
+
+public class ListaSimplementeEnlazada {
+// tail no es necesaria en la 
+// lista simplemente enlazada, solo se enfoca en cabeza (head)
+
+    public class Node {
+
+        int data;
+        Node next;
+
+        Node(int d) {
+            data = d;
+
+        }
+
+    }
+    Node head;
+
+    public ListaSimplementeEnlazada insertar(ListaSimplementeEnlazada list, int data) {
+        // crear un nuevo nodo                                
+        Node new_node = new Node(data);
+        new_node.next = null;
+
+        // Para el caso que la lista este vacia
+        if (list.head == null) {
+            list.head = new_node;
+        } else {
+            // para ir al final de la lista
+            Node last = list.head;
+            while (last.next != null) {
+                last = last.next;
+            }
+            // insertar al final de la lista
+            last.next = new_node;
+
+        }
+        return list;
+    }
+
+    public void print(ListaSimplementeEnlazada list) {
+
+        
+
+    }
+
+}
