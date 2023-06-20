60f3b2c432a7486bef97ab49d20a1fcb9c2aa3ee
Carlos Mejia <cdmejia4@utpl.edu.ec>
2023-06-19 21:57:02 -0500

    clases con listas circulares simples y dobles

--- Clases/src/semana11/ListaCircularSimplementeEnlazada.java
+++ Clases/src/semana11/ListaCircularSimplementeEnlazada.java
@@ -46,37 +46,8 @@ public class ListaCircularSimplementeEnlazada {
 
     }
 
-     public ListaSimplementeEnlazada insertar(ListaSimplementeEnlazada list, int data) {
-        // crear un nuevo nodo                                
-        Node new_node = new Node(data);
-        new_node.next = null;
-
-        // Para el caso que la lista este vacia
-        if (list.head == null) {
-            list.head = new_node;
-        } else {
-            // para ir al final de la lista
-            Node last = list.head;
-            while (last.next != null) {
-                last = last.next;
-            }
-            // insertar al final de la lista
-            last.next = new_node;
-
-        }
-        return list;
-    }
-
-    public void print(ListaSimplementeEnlazada list) {
-        Node currentNode = list.head;
-        System.out.println("Lista enlazada simple: ");
-        while(currentNode != null){
-            System.out.printf("[%d] ", currentNode.data);
-            currentNode = currentNode.next;
-        }
-        
-        System.out.println("");
-    }
+    
+    // borrar un numero (modificar el de abajo)
     
     public ListaCircularSimplementeEnlazada delete (ListaCircularSimplementeEnlazada list, int key){
         // key is the deleting number
