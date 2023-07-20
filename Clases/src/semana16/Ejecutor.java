c8f121094d0416c0bf149bddaed5ac301c5624ab
Carlos Mejia <cdmejia4@utpl.edu.ec>
2023-07-20 16:14:38 -0500

    subida final

---
+++ Clases/src/semana16/Ejecutor.java
@@ -0,0 +1,72 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package semana16;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ *
+ * @author omerb
+ */
+public class Ejecutor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        GrafoDirigido grafo = new GrafoDirigido(10);
+
+        grafo.agregarArista(0, 1);
+        grafo.agregarArista(0, 2);
+        grafo.agregarArista(0, 3);
+        grafo.agregarArista(2, 4);
+        grafo.agregarArista(1, 3);
+        grafo.agregarArista(4, 5);
+        grafo.agregarArista(3, 5);
+        grafo.agregarArista(1, 4);
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("---------------------------");
+            List<Integer> adyacentes = grafo.obtenerAdyacentes(i);
+            System.out.printf("Nodos adyacentes al nodo %d:\n", i);
+
+            for (Integer nodo : adyacentes) {
+                System.out.println(nodo);
+            }
+
+        }
+        System.out.println("---------------------------");
+
+        System.out.println("Nodos del Grafo");
+        // Realizar el recorrido en anchura (BFS)
+        bfs(grafo.getAdyacencia(), 0);
+        System.out.println("---------------------------");
+
+    }
+
+    public static void bfs(List<List<Integer>> grafo, int nodoInicial) {
+        boolean[] visitados = new boolean[grafo.size()];
+        Queue<Integer> cola = new LinkedList<>();
+
+        visitados[nodoInicial] = true;
+        cola.offer(nodoInicial);
+
+        while (!cola.isEmpty()) {
+            int nodoActual = cola.poll();
+            System.out.println(nodoActual);
+
+            for (int adyacente : grafo.get(nodoActual)) {
+                if (!visitados[adyacente]) {
+                    visitados[adyacente] = true;
+                    cola.offer(adyacente);
+                }
+            }
+        }
+    }
+
+}
---
+++ Clases/src/semana16/GrafoDirigido.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package semana16;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author omerb
+ */
+public class GrafoDirigido {
+    private int numNodos;
+    private List<List<Integer>> adyacencia;
+
+    public GrafoDirigido(int numNodos) {
+        this.numNodos = numNodos;
+        this.adyacencia = new ArrayList<>();
+
+        for (int i = 0; i < numNodos; i++) {
+            adyacencia.add(new ArrayList<>());
+        }
+    }
+
+    public void agregarArista(int origen, int destino) {
+        adyacencia.get(origen).add(destino);
+       adyacencia.get(destino).add(origen);//Agregamos esta linea se convierte en no dirigido
+        
+    }
+
+    public List<List<Integer>> getAdyacencia() {
+        return adyacencia;
+    }
+
+    public List<Integer> obtenerAdyacentes(int nodo) {
+        return adyacencia.get(nodo);
+    }
+}
